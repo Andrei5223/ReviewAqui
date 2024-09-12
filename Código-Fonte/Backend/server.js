14d8971d457f84bda095594577eefb490a5624cf
erickson-cc <>
2024-09-12 17:41:26 -0300

    Formatação Frontend

--- Código-Fonte/Backend/server.js
+++ Código-Fonte/Backend/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const pgp = require("pg-promise")({});  
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(cors())
+
+const usuario = "postgres";
+const senha = "postgres";
+const db = pgp(`postgres://${usuario}:${senha}@localhost:5432/reviewaki`);
 
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
@@ -11,5 +17,25 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
-  });
+  res.send("Hello world");
+});
+
+app.post("/cadastro", async (req, res) => {
+  try {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    console.log(nome);
+
+    db.none(
+      "INSERT INTO pessoa (nome, email, senha) VALUES ($1, $2, $3);",
+      [nome, email, senha]
+    );
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
--- Código-Fonte/Frontend/src/components/Cadastro.jsx
+++ Código-Fonte/Frontend/src/components/Cadastro.jsx
@@ -48,38 +48,69 @@ export default function Cadastro() {
         height: '100vh', // Para centralizar verticalmente, você pode usar a altura da viewport
       }}
     >
-      <Card component="section" sx={{ p: 2, border: '1px grey', maxWidth: 800 }}>
+      <Stack direction="row">
+      <Card
+      sx={{
+        backgroundColor: '#68fcad',
+        minWidth: 250,
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        padding: 2 
+      }}
+    >
+      <Typography variant="h5" component="h1" gutterBottom>
+        Já tem uma conta?
+      </Typography>
+
+      <Button
+        variant='contained'
+        sx={{ backgroundColor: 'white', color: 'black' }}
+      >
+        Entrar
+      </Button>
+    </Card>
+
+      <Card component="section" sx={{ p: 2, border: '1px grey', maxWidth: 800, minWidth: 450}}>
         <CardContent>
-        <Typography variant="h4" component="h2">
+
+          <Stack spacing={3} direction="column">
+        <Typography variant="h4" component="h2" fontFamily={'Arial, Helvetica, sans-serif'}>
             Crie Sua Conta
         </Typography>
-
-          <Stack spacing={2} direction="column">
             <TextField 
               id="filled-basic" 
               label="Nome" 
+              required
               variant="filled"
               onChange={(e) => setNome(e.target.value)}
               value={nome}
-            />
+              />
             <TextField 
               id="filled-basic" 
               label="E-Mail" 
-              variant="filled" 
+              required 
+              variant="filled"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
             />
             <TextField 
               id="filled-basic" 
               label="Senha" 
+              type='password'
+              required
               variant="filled"
               onChange={(e) => setSenha(e.target.value)}
               value={senha}
             />
+          <Button variant="contained" sx={{ mt: 2, backgroundColor: '#68fcad', color: 'black', maxWidth: '100px'}} onClick={handleSubmit}>Cadastrar</Button>
           </Stack>
-          <Button variant="contained" sx={{ mt: 2 }} onClick={handleSubmit}>Cadastrar</Button>
         </CardContent>
       </Card>
+    </Stack>
     </Box>
   );
 }
