f883cfe6f84d2f4751458e9bb0010be263028143
Andrei5223 <109106968+Andrei5223@users.noreply.github.com>
2024-09-12 17:04:43 -0300

    Integração do frontend com backend
    Requisições do frontend

--- Código-Fonte/Frontend/src/components/Cadastro.jsx
+++ Código-Fonte/Frontend/src/components/Cadastro.jsx
@@ -6,8 +6,37 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import axios from "axios";
 
 export default function Cadastro() {
+
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [senha, setSenha] = React.useState("");
+
+  function clearForm() {
+    setNome("");
+    setEmail("");
+    setSenha("");
+  }
+
+  async function handleSubmit() {
+
+    if (nome !== "" && email !== "" && senha !== "") {
+      try {
+          await axios.post("/cadastro", {
+              nome: nome,
+              email: email,
+              senha: senha,
+          });
+          console.log(`Nome: ${nome} - Email: ${email}`);
+          clearForm(); // limpa o formulário apenas se cadastrado com sucesso
+      } catch (error) {
+          console.log(error);
+      }
+    }
+  }
+
   return (
     <Box
       sx={{
@@ -24,11 +53,29 @@ export default function Cadastro() {
         </Typography>
 
           <Stack spacing={2} direction="column">
-            <TextField id="filled-basic" label="Nome" variant="filled" />
-            <TextField id="filled-basic" label="E-Mail" variant="filled" />
-            <TextField id="filled-basic" label="Senha" variant="filled" />
+            <TextField 
+              id="filled-basic" 
+              label="Nome" 
+              variant="filled"
+              onChange={(e) => setNome(e.target.value)}
+              value={nome}
+            />
+            <TextField 
+              id="filled-basic" 
+              label="E-Mail" 
+              variant="filled" 
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <TextField 
+              id="filled-basic" 
+              label="Senha" 
+              variant="filled"
+              onChange={(e) => setSenha(e.target.value)}
+              value={senha}
+            />
           </Stack>
-          <Button variant="contained" sx={{ mt: 2 }}>Cadastrar</Button>
+          <Button variant="contained" sx={{ mt: 2 }} onClick={handleSubmit}>Cadastrar</Button>
         </CardContent>
       </Card>
     </Box>
