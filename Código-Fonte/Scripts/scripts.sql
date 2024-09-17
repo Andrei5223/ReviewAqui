e9eb9849bac492689545078062f74faf4a814237
Ca-olho <79473720+Ca-olho@users.noreply.github.com>
2024-09-17 14:43:54 -0300

    alteração do banco
    remoção do ID e Email como PK

--- Código-Fonte/Scripts/scripts.sql
+++ Código-Fonte/Scripts/scripts.sql
@@ -2,11 +2,10 @@ CREATE DATABASE reviewaki;
 
 \c reviewaki
 
-CREATE TABLE pessoa (  
-idp SERIAL PRIMARY KEY,
+CREATE TABLE pessoa (
+email VARCHAR(51) NOT NULL PRIMARY KEY,  
 nome VARCHAR(51) NOT NULL,
-email VARCHAR(51) NOT NULL,
 senha VARCHAR(251) NOT NULL
 );
 
-insert into pessoa (nome, email, senha) values ('Andrei', 'andrei.teste@gmail.com', '1234');
+--insert into pessoa (nome, email, senha) values ('Andrei', 'andrei.teste@gmail.com', '1234');
