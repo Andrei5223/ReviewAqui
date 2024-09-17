e76a076b8d8602dab59cdf0daf2fc016cb140c30
erickson-cc <>
2024-09-17 14:59:47 -0300

    Segurança do cadastro no Backend

--- Código-Fonte/Backend/server.js
+++ Código-Fonte/Backend/server.js
@@ -7,6 +7,29 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors())
 
+// Segurança para login
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+app.use(
+	session({
+		secret: 'Segredo_mt_secreto',
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: true },
+	}),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const requireJWTAuth = passport.authenticate("jwt", { session: false });
+
 const usuario = "postgres";
 const senha = "postgres";
 const db = pgp(`postgres://${usuario}:${senha}@localhost:5432/reviewaki`);
@@ -21,21 +44,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/cadastro", async (req, res) => {
+  const { nome, email, senha } = req.body;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const saltRounds = 10;
+
+  // Valida entradas (Para caso de uso de proxy)
+  if (!nome || !email || !senha) {
+    console.log("Nome, email e senha são obrigatórios");
+    return res.status(400).json({ error: "Nome, email e senha são obrigatórios" });
+  }
+  if (nome.length < 3 || nome.length >= 50 || senha.length < 8 || senha.length > 30){
+    console.log("Tamanho inválido de senha ou nome");
+    return res.status(400).json({ error: "Tamanho inválido de senha ou nome" });
+  }
+  if (!emailPattern.test(email)){
+    console.log("Formato de email inválido");
+    return res.status(400).json({ error: "Formato de email inválido" });
+  }
+
+  // Criptografa a senha e insere no banco
   try {
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const senha = req.body.senha;
-
-    console.log(nome);
-
-    db.none(
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(senha, salt);
+    await db.none(
       "INSERT INTO pessoa (nome, email, senha) VALUES ($1, $2, $3);",
-      [nome, email, senha]
+      [nome, email, hash]
     );
-
     res.sendStatus(200);
   } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
+    console.error(error.message);
+    res.sendStatus(500); 
   }
 });
