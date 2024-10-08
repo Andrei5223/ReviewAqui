a703610f583bd18531fc8999d1bd1ed114aacfb3
YuriLanzini <103113683+YuriLanzini@users.noreply.github.com>
2024-10-08 15:32:44 -0300

    update scripts.sql

--- Código-Fonte/Scripts/scripts.sql
+++ Código-Fonte/Scripts/scripts.sql
@@ -3,9 +3,27 @@ CREATE DATABASE reviewaki;
 \c reviewaki
 
 CREATE TABLE pessoa (
-email VARCHAR(51) NOT NULL PRIMARY KEY,  
-nome VARCHAR(51) NOT NULL,
-senha VARCHAR(251) NOT NULL
+    email VARCHAR(51) NOT NULL PRIMARY KEY,  
+    nome VARCHAR(51) NOT NULL,
+    senha VARCHAR(251) NOT NULL
 );
 
---insert into pessoa (nome, email, senha) values ('Andrei', 'andrei.teste@gmail.com', '1234');
+CREATE TABLE produto (
+    idp SERIAL PRIMARY KEY,  
+    nome VARCHAR(51) NOT NULL
+);
+
+CREATE TABLE fonte (
+    idf SERIAL PRIMARY KEY,  
+    nome VARCHAR(51) NOT NULL,
+    link VARCHAR(501) NOT NULL,
+    idp INT,
+    CONSTRAINT fk_produto FOREIGN KEY (idp) REFERENCES produto(idp)
+);
+
+CREATE TABLE imagem (
+    idi SERIAL PRIMARY KEY,  
+    link VARCHAR(501) NOT NULL,
+    idp INT,
+    CONSTRAINT fk_produto FOREIGN KEY (idp) REFERENCES produto(idp)
+);
